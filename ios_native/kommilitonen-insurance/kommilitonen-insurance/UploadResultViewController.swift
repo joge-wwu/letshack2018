e8cc09cadf1058d76617f4a1691913cfce1ffdc3
Johannes Voscort <johannesvoscort@Johanness-MacBook-Pro.local>
2018-01-13 12:59:17 +0100

    new stuff

---
+++ ios_native/kommilitonen-insurance/kommilitonen-insurance/UploadResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UploadResultViewController.swift
+//  kommilitonen-insurance
+//
+//  Created by Johannes Voscort on 13.01.18.
+//  Copyright © 2018 Die Kommilitonen. All rights reserved.
+//
+
+import Foundation
